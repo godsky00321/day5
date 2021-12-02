2b9e2b3df3efd7c7ef62d0d40f88fd66a2d0ffcd
godsky00321 <64309010002@chontech.ac.th>
2021-12-02 16:37:51 +0700

    d

--- showdemo.php
+++ showdemo.php
@@ -10,7 +10,7 @@ $dbname = "it64309010015";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
+mysqli_set_charset($conn,"utf8");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
