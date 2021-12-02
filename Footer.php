f64364684d1808516b37b4c6dc3cf993520bf45c
godsky00321 <64309010002@chontech.ac.th>
2021-12-02 11:27:09 +0700

    day5

--- Footer.php
+++ Footer.php
@@ -4,5 +4,4 @@
 <div class="jumbotron text-center" style="padding-top:5px; padding-bottom:5px; margin-bottom:0px;"><br>
 <p>จัดทำโดย Phichaiyut Watsaro เทคโนโลยีสารสนเทศ สทส.3/1</p>
 </div>
-
 </body>
